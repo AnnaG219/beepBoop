fbfbe76c21b9d62d1e03daa13e3161c5945b3061
Anna Gibbons <anna@gibbons-clan.com>
2019-04-05 16:42:19 -0700

    add more changes to js and readme

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,20 @@
-$(document).ready(function(){
-
-  $("form#convert").submit(function(event){
+$(document).ready(function() {
+  $("#convertform").submit(function(event) {
     event.preventDefault();
-
-      var userNumber = parseInt($("#numberChoiceInput").val());
-      var result = beepBoop(userNumber);
-
-    function beepBoop(userNumber){
-      for(var i = 0; i <= userNumber.length; i++) {
-      //  userNumber.toString()
-        $("#answerList").text("<li>"+i+"</li>");
-      }
+    var userNumber = parseInt($("#numberChoiceInput").val());
+    var result =(userNumber);
+    $("#result").text(result);
+    var result = function(userNumber) {
+      return false
     }
   });
 });
+
+//  var userString = userNumber.toString();
+  //  var stringChange = ["Beep","Boop", "I'm sorry Dave, I'm afraid I can't do that."]
+
+      //  userNumber.toString()
+      //$("#answerList").text("<li>"+i+"</li>");
+    //}
+//    }
+//  });
