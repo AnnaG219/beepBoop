06bba331b002c3729c2bb62b6a5aa879c3bb3d36
Anna Gibbons <anna@gibbons-clan.com>
2019-04-07 17:29:01 -0700

    add a form input for user name, and play with css colors and design.

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,21 @@
 $(document).ready(function() {
   $("#translate").submit(function(event) {
     event.preventDefault();
-    var userNumber = parseInt($("#userNum").val());
     var result = ["Beep","Boop","I'm sorry Dave, I'm afraid I can't do that."]
-
     $("#translatedList").text("");
+
+    var name = $("#userName").val();
+    var userNumber = parseInt($("#userNum").val());
+
+    if (name === "" || $("#userNum").val() === "" ) {
+      alert("Sorry, you have to type in your name, or any name, and a number, any number!")
+      return;
+    }
 
     for (var i = 0; i <= userNumber; i++) {
       var displayText = String(i);
       if (displayText.indexOf("3") !== -1) {
-        displayText = result[2];
+        displayText = result[2].replace("Dave", name);
       }
       else if (displayText.indexOf("2") !== -1) {
         displayText = result[1];
